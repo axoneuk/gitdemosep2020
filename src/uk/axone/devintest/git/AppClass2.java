0bcac3b28938eb0f871da5ae362bcf206ab272f7
ramduvvuri <ram@axone-tech.uk>
2020-09-20 08:33:50 +0100

    Jira 1234 - Added new method to AppClass2.java

--- src/uk/axone/devintest/git/AppClass2.java
+++ src/uk/axone/devintest/git/AppClass2.java
@@ -2,8 +2,12 @@ package uk.axone.devintest.git;
 
 public class AppClass2 {
 
-    public void method1(){
+    public void method1() {
         System.out.println("Doing something");
+    }
+
+    public void method2(){
+        System.out.println("Doing something new");
     }
 
 }
